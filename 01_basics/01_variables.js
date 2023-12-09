6da90b36afcf005067928a30af7ee72ce980e08a
Sudheer Chauhan <65271198+Sudheer-Chauhan@users.noreply.github.com>
2023-12-09 20:19:43 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 789456;
+let accountEmail = "sudheer@accountEmail.com"
+var accountPassword = "123456"
+accountCity = "Jodhpur"
+let accountState
+
+console.log(accountId);
+
+accountEmail = "sc@.com"
+accountPassword = 85246
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
